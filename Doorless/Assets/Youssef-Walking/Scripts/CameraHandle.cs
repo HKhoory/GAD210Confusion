45957b72f1bce6b184d7aa2ea300c5906641692c
youssef waeil <ywaeil938@gmail.com>
2024-11-03 21:03:20 +0400

    Created 2 Scripits
    Camera and movement to handle movemnt

--- Doorless/Assets/Youssef-Walking/Scripts/CameraHandle.cs
+++ Doorless/Assets/Youssef-Walking/Scripts/CameraHandle.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class CameraHandle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // verti and hori input values
+    public float verticalAxis;
+    public float horizontalAxis;
+
+    // mouse movemnt for camera rotation
+    private float MouseX;
+    private float MouseY;
+
     void Start()
     {
-        
+        //locks the cursor int the middle and makes the mouse curor disapper
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //call Moving function
+        Moving();
+
+        //get mouse movemnts 
+        MouseX += Input.GetAxis("Mouse X");
+        MouseY += Input.GetAxis("Mouse Y");
+
+        //limits camaera movment angle
+        MouseY = Mathf.Clamp(MouseY, -45, 60);
+
+        //handles rotation
+        transform.rotation = Quaternion.Euler(0, MouseX, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(-MouseY, MouseX, 0);
+    }
+
+    private void Moving()
+    {
+        //vertical axis for W and S keys or up and down arrows
+        //horizontal axis for A and D keys or left and right arrows
+
+        verticalAxis = Input.GetAxis("Vertical");
+        horizontalAxis = Input.GetAxis("Horizontal");
+
     }
 }
--- Doorless/Assets/Youssef-Walking/Scripts/Movement.cs
+++ Doorless/Assets/Youssef-Walking/Scripts/Movement.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 5f;
+    private Vector3 direction;
+
+    private Transform mainCam;
+
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        mainCam = Camera.main.transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        DirectionMovement();
+    }
+
+    // calculates movemnt direction 
+    public void DirectionMovement()
+    {
+        float vAxis = Input.GetAxis("Vertical");
+        float hAxis = Input.GetAxis("Horizontal");
+
+        direction = (mainCam.forward * vAxis) + (mainCam.right * hAxis);
+
+        direction.Normalize();
+        direction.y = 0;
+
+        direction *= speed;
+
+        rb.velocity = direction;
     }
 }
