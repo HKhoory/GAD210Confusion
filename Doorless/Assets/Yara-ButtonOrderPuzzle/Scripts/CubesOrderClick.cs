d79ca258781a679418bec5d958f318e18b87f19c
YaraSAE <y.raffoul@student.saedubai.com>
2024-11-04 14:29:26 +0400

    Puzzle: Sound order

---
+++ Doorless/Assets/Yara-ButtonOrderPuzzle/Scripts/CubesOrderClick.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubesOrderClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+
+    public List<string> correctOrderTags = new List<string> { "Sound1", "Sound2", "Sound3","Sound4","Sound5" }; // Replace with your actual object names or tags
+    private List<string> clickedOrderTags = new List<string>();
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Check for mouse click
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                string clickedNameTag = hit.transform.gameObject.tag;
+                clickedOrderTags.Add(clickedNameTag);
+
+                // Check if the clicked order matches the correct order
+                if (clickedOrderTags.Count == correctOrderTags.Count)
+                {
+                    bool correct = true;
+                    for (int i = 0; i < correctOrderTags.Count; i++)
+                    {
+                        if (clickedOrderTags[i] != correctOrderTags[i])
+                        {
+                            correct = false;
+                            break;
+                        }
+                    }
+
+                    if (correct)
+                    {
+                        Debug.Log("Clicked in correct order!");
+                    }
+                    else
+                    {
+                        Debug.Log("Clicked in incorrect order.");
+                    }
+
+                    // Clear the list for the next try
+                    clickedOrderTags.Clear();
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Doorless/Assets/Yara-ButtonOrderPuzzle/Scripts/SoundPlay.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlay : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The start function is called");
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Check for mouse click
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                string clickedNameTag = hit.transform.gameObject.tag;
+
+                switch (clickedNameTag)
+                {
+                    case "Sound1":
+                    audioSource.pitch = 0.5f;
+                    break;
+                    case "Sound2":
+                    audioSource.pitch = 1f;
+                    break;
+                    case "Sound3":
+                    audioSource.pitch = 1.5f;
+                    break;
+                    case "Sound4":
+                    audioSource.pitch = 2f;
+                    break;
+                    default:
+                    audioSource.pitch = 2.5f;
+                    break;
+                }
+                audioSource.Play();
+                
+            }
+        }
+    }
+}
