318d4a95e0a86129ff737f40e7abca668cf54922
femixlr8 <fathima.latheef@student.saedubai.com>
2024-11-09 21:09:30 +0400

    added sfx
    added bg music (might change the audio), win condition sfx and coin collection sfx for the coin collection mechanic that will be set up

---
+++ Doorless/Assets/Fathima/Scripts/CoinCollector.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CoinCollector : MonoBehaviour
+{
+    public AudioClip coinSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            audioSource.PlayOneShot(coinSound);
+            Destroy(other.gameObject); // Destroy the coin after collection
+        }
+    }
+}
